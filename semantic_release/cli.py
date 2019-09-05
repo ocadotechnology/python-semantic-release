27aa24ea27a1ef2706ed04903b719214710e1144
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-09-05 18:17:58 +0100

    Put version commit and tag in original spots.

--- semantic_release/cli.py
+++ semantic_release/cli.py
@@ -99,17 +99,21 @@ def version(**kwargs):
             # No need to make changes to the repo, we're just retrying.
             return True
 
+        commit_new_version(new_version)
+        tag_new_version(new_version)
+        set_new_version(new_version)
+
         click.echo('Bumping with a {0} version to {1}.'.format(level_bump, new_version))
 
     else:
         new_version = master_version + ".dev" + build
+
+        tag_new_version(new_version)
+        set_new_version(new_version)
+
         click.echo('Not bumping as this is a dev build.')
 
     click.echo('{0} Committing and tagging new version: '.format(new_version))
-
-    commit_new_version(new_version)
-    tag_new_version(new_version)
-    set_new_version(new_version)
 
     return True
 
